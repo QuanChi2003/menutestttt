31a6c47f69cd358020f4011ebd40e16db7af63fa
QuanChi2003 <109083423+QuanChi2003@users.noreply.github.com>
2025-10-01 20:40:11 +0700

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,5 @@
+'use client';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from './types'
+
+export const supabase = createClientComponentClient<Database>()
